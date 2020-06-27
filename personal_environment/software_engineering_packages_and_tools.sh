a455c78bb7e52675c52667be3ea86c13379daee4
Willian Antunes <willian.lima.antunes@gmail.com>
2020-06-27 05:09:10 -0700

    GCP part, but it's incomplete yet

--- personal_environment/software_engineering_packages_and_tools.sh
+++ personal_environment/software_engineering_packages_and_tools.sh
@@ -117,5 +117,15 @@ function install_software_engineering_tools() {
   chmod +x ./aws-iam-authenticator
 
   echo "<<< GCP - TODO"
+
+  # Add the Cloud SDK distribution URI as a package source
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+  # Import the Google Cloud Platform public key
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+  # Update the package list and install the Cloud SDK
+  sudo apt-get update && sudo apt-get install google-cloud-sdk
+
   echo "<<< Azure - TODO"
 }
